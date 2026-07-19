97f1dbc1084df2601b0149b00bfbf71a87755c6b
Able Thomas <sanjuthomas467@gmail.com>
2026-07-19 10:37:09 +0530

    edit

--- src/report.py
+++ src/report.py
@@ -1,23 +1,12 @@
 def generate_report(prob1,prob0):
-
     print()
-
     print("Probability of Diabetes")
-
     print(round(prob1*100,2),"%")
-
     print()
-
     print("Probability of Non Diabetes")
-
     print(round(prob0*100,2),"%")
-
     print()
-
     if prob1>prob0:
-
         print("Prediction : Diabetic")
-
     else:
-
         print("Prediction : Non Diabetic")
