b08000fa71cd9b2ca6e8296cc4b321716f16688e
Able Thomas <sanjuthomas467@gmail.com>
2026-07-19 10:36:43 +0530

    edit

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,11 +1,7 @@
 import numpy as np
-
 def preprocess(data):
-    # Replace impossible 0 values with NaN
     columns = ["Glucose", "BloodPressure", "SkinThickness", "Insulin", "BMI"]
-
     for col in columns:
         data[col] = data[col].replace(0, np.nan)
         data[col].fillna(data[col].mean(), inplace=True)
-
     return data
