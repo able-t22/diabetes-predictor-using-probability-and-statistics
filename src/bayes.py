46fcc63178500e1f3d896f00a4bc66b6591b917c
Able Thomas <sanjuthomas467@gmail.com>
2026-07-19 10:34:32 +0530

    Clean up whitespace in BayesianPredictor class
    Refactor BayesianPredictor class by removing unnecessary whitespace and improving code readability.

--- src/bayes.py
+++ src/bayes.py
@@ -1,41 +1,23 @@
 import pandas as pd
-
 from gaussian import gaussian_probability
 class BayesianPredictor:
-
     def __init__(self,data):
-
         self.data=data
-
         self.diabetic=data[data["Outcome"]==1]
-
         self.normal=data[data["Outcome"]==0]
-
         self.prior_diabetic=len(self.diabetic)/len(data)
-
         self.prior_normal=len(self.normal)/len(data)
     def predict(self,sample):
-
         diabetic_prob=self.prior_diabetic
-
         normal_prob=self.prior_normal
-
         for feature,value in sample.items():
-
             mean1=self.diabetic[feature].mean()
             std1=self.diabetic[feature].std()
-
             mean0=self.normal[feature].mean()
             std0=self.normal[feature].std()
-
             diabetic_prob*=gaussian_probability(value,mean1,std1)
-
             normal_prob*=gaussian_probability(value,mean0,std0)
-
         total=diabetic_prob+normal_prob
-
         diabetic_prob/=total
-
         normal_prob/=total
-
         return diabetic_prob,normal_prob
